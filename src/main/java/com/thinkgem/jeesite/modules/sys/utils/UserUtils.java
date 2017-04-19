9c117085a95e47e41d8affc0af3f63bde5d4e0fa
wangjie <wangjie@wondersgroup.com>
2017-04-19 09:48:50 +0800

    获取角色名称

--- src/main/java/com/thinkgem/jeesite/modules/sys/utils/UserUtils.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/utils/UserUtils.java
@@ -19,6 +19,7 @@ import org.mybatis.spring.DataSource;
 import org.mybatis.spring.DataSourceContextHolder;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -334,6 +335,64 @@ public class UserUtils {
 			
 		}
 		return null;
+	}
+
+	/**
+	 * 获取当前登录用户的所有信息 no(工号) name(昵称) jgType(机构类型sfy-qfy-zcjg-sq) jgCode(机构编码)
+	 * jgName(机构名称) qxCode(所属区县编码) qxName(所属区县名称) jgGrade（2 二级，3三级）(机构级别)
+	 */
+	public Map<String, String> getOnline() {
+		Map<String, String> m = new HashMap<String, String>();
+		// 登录数据
+		User u = getUser();
+		m.put("no", u.getId());
+		m.put("name", u.getName());
+		m.put("loginName", u.getLoginName());
+
+
+		// 机构数据
+		Office o = u.getCompany();
+		m.put("jgCode", o.getCode());
+		m.put("jgName", o.getName());
+
+		List<Role> rList = u.getRoleList();
+		Map<String, String> rMap = Maps.newHashMap();
+		for (Role r : rList) {
+			rMap.put(r.getEnname(), null);
+		}
+
+		if (rMap.containsKey("fyzx")) {
+			m.put("jgType", "sfy");
+		} else if (rMap.containsKey("qxfy")) {
+			m.put("jgType", "qfy");
+		} else if (rMap.containsKey("zcjg")) {
+			m.put("jgType", "zcjg");
+		} else if (rMap.containsKey("sqzx")) {
+			m.put("jgType", "sq");
+		} else if (rMap.containsKey("ptyy")) {
+			m.put("jgType", "ptyy");
+		} else if (rMap.containsKey("zdzx")) {
+			m.put("jgType", "zdzx");
+		} else if (rMap.containsKey("SCZX")) {
+			m.put("jgType", "sczx");
+		} else if (rMap.containsKey("fkyy")) {
+			m.put("jgType", "fkyy");
+		}
+
+		if ("3".equals(o.getGrade())) {
+			m.put("jgGrade", "3");
+		} else {
+			if ("2".equals(o.getGrade())) {
+				m.put("jgGrade", "2");
+			} else {
+				m.put("jgGrade", "");
+			}
+		}
+
+		// 所属区县
+		m.put("qxCode", o.getArea().getCode());
+		m.put("qxName", o.getArea().getName());
+		return m;
 	}
 	
 	public static Session getSession(){
